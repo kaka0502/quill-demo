7576600cfa563b04eda97b543889f346af151e39
Kalyan Kattamuri <Kattamuri.Kalyan@mayo.edu>
2021-05-21 01:03:20 -0500

    Quill Editor added with default config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RichTextEditorModule } from './rich-text-editor/rich-text-editor.module';
+import { QuillModule } from "ngx-quill";
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RichTextEditorModule,
+    QuillModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
